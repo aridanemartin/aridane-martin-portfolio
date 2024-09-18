8f6c0afcea4c03fcc6d144e781a4157847275723
Aridane Martín <aridanemartin@gmail.com>
2024-09-18 21:57:55 +0100

    New Blog Page / Timeline added to about section

---
+++ src/content/about/timelineContent.ts
@@ -0,0 +1,26 @@
+export const timelineContent = [
+  {
+    title: "Milestone 1",
+    year: "2021",
+    description:
+      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
+    image: "/assets/aridaneLogo.webp",
+    alt: "Aridane Martín",
+  },
+  {
+    title: "Milestone 2",
+    year: "2021",
+    description:
+      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
+    image: "/assets/aridaneLogo.webp",
+    alt: "Aridane Martín",
+  },
+  {
+    title: "Milestone 3",
+    year: "2021",
+    description:
+      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
+    image: "/assets/aridaneLogo.webp",
+    alt: "Aridane Martín",
+  },
+];
