abcc59defe730fea86f56a5c40c46f9fb887d026
Aridane Martín <aridanemartin@gmail.com>
2024-09-20 20:36:04 +0100

    Picture assets / Creation of blog section

--- src/content/about/timelineContent.ts
+++ src/content/about/timelineContent.ts
@@ -1,26 +1,46 @@
 export const timelineContent = [
   {
-    title: "Milestone 1",
-    year: "2021",
+    title: "Openbank",
+    year: "2024",
     description:
-      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
-    image: "/assets/aridaneLogo.webp",
-    alt: "Aridane Martín",
+      "As part of the consulting company I work for (Knowmadmood), I was assigned to Openbank, a digital bank that operates in Spain. I was responsible for developing and maintaining the front-end of the Call Center build in hexagonal architecture and event driven design using react and typescript as the chosen stack in a challenging project.",
+    image: "/assets/timeline/SCNPA.webp",
+    imageDescription:
+      "With my dear friends Juanma and Carlos at the SCNPA conference.",
   },
   {
-    title: "Milestone 2",
-    year: "2021",
+    title: "SpainSkills",
+    year: "2024",
     description:
-      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
-    image: "/assets/aridaneLogo.webp",
-    alt: "Aridane Martín",
+      "In collaboration with 'El Rincón', I participated as a tutor preparing the award winning student, Kilian Sosa to compete in the SpainSkills competition, a national competition that showcases the skills of young professionals in various fields. ",
+    image: "/assets/timeline/kilian.webp",
+    imageDescription:
+      "With my student Kilian attending a developers conference",
   },
   {
-    title: "Milestone 3",
+    title: "Douglas",
+    year: "2022",
+    description:
+      "I joined Douglas a leading beauty and cosmetics retailer in Europe, as a front-end developer as part of the consulting company Rindus. I was responsible for developing and maintaining the front-end of the company's e-commerce platform in 7 countries (Germany, France, Belgium, Switzerland, Poland, Austria, and Spain).",
+    image: "/assets/timeline/douglas.webp",
+    imageDescription:
+      "With team Yoda and team Sunny in JSDay Canarias, a conference for JavaScript developers.",
+  },
+  {
+    title: "Secret Source",
     year: "2021",
     description:
-      "I started my journey as a full-stack developer, working on both the front-end and back-end of web applications, creating user-friendly interfaces and ensuring smooth server-side performance. My ability to handle the entire development process allows me to deliver efficient, scalable, and visually engaging solutions that meet both technical and business goals.",
-    image: "/assets/aridaneLogo.webp",
-    alt: "Aridane Martín",
+      "I joined Secret Source, a digital agency based in the UK, as a front-end developer. I was responsible for developing a web app for NOCN collaborating with the design team to implement new designs and improve the overall user experience of the app.",
+    image: "/assets/timeline/secret.webp",
+    imageDescription: "With my dear colleagues of Secret Source.",
+  },
+  {
+    title: "EscolaPort BCN",
+    year: "2020",
+    description:
+      "I joined EscolaPort Barcelona, a sailing school based in Barcelona, as a full-stack developer. I was responsible for developing and maintaining the front-end and backend of the company's booking app, ensuring that it is user-friendly, responsive, and visually appealing.",
+    image: "/assets/timeline/esteban.webp",
+    imageDescription:
+      "With my dear friend Esteban in some of the Gofigeeks mensual events.",
   },
 ];
